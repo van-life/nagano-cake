2a6abd7b697bf13747fe0b5322b67071ee69cedf
yanmasa <ueda3ban@gmail.com>
2021-10-18 03:19:58 +0000

    customersのVCに記述

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -1,22 +1,43 @@
 class Customer::CustomersController < ApplicationController
+  before_action :authenticate_customer!, except: [:top,:about]
+  
   def top
+    @items = Item.where(status: true).order(created_at: :desc).limit(4)
+    # 全商品か、販売中のものでallかwhere(status: true)、順番は降順:desc,昇順:asc
   end
+  
   def about
   end
   
   def show
+    @customer = current_customer
   end
   
   def quit
   end
   
   def out
+    current_customer.update(is_deleted: false)
+    redirect_to root_path
   end
   
   def edit
+    @customer = current_customer
   end
   
   def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to customers_path(current_customer)
+    else
+      render :edit
+    end
+  end
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:first_name,:last_name,:first_name_kana,:last_name_kana,:postal_code,:adress,:telephone_number,:email)
   end
 
 end
