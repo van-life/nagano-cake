ff0e0ace9d69858cfd44eacba916d1dea4d85134
yanmasa <ueda3ban@gmail.com>
2021-10-18 00:03:45 +0000

    customersコントローラ、ビューファイルの作成

---
+++ app/controllers/customer/customers_controller.rb
@@ -0,0 +1,22 @@
+class Customer::CustomersController < ApplicationController
+  def top
+  end
+  def about
+  end
+  
+  def show
+  end
+  
+  def quit
+  end
+  
+  def out
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+
+end
