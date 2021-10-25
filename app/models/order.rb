9bc4d8368a86ffb25a838c0d1317b667f37e1c94
yanmasa <ueda3ban@gmail.com>
2021-10-25 04:37:35 +0000

    バリデーションの修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_items, dependent: :destroy
 
-  validates :address,:name,:postage,:total_price, presence: true
+  validates :address,:name,:postage,:postal_code,:total_price, presence: true
 
   enum payment: {クレジットカード: 0, 銀行振込: 1}
 
